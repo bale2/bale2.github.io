ccbb8d79e0d75f064f03c69c7b6c0e2dbf499746
bale2 <44296032+bale2@users.noreply.github.com>
2023-09-20 21:23:21 +0200

    i hope it is done

--- script/script.js
+++ script/script.js
@@ -7,6 +7,16 @@ function pulseImage(image) {
     }, 200); 
 }
 
+function accessibilityToggle() {
+    var ac = document.getElementById("accessibility");
+    if (ac.disabled === true) {
+        ac.disabled = false;
+    }
+    else {
+        ac.disabled = true;
+    }
+}
+
 pulsatingImages.forEach((image) => {
     image.addEventListener('mouseenter', () => {
         const intervalId = setInterval(() => pulseImage(image), 800);
@@ -15,4 +25,9 @@ pulsatingImages.forEach((image) => {
             image.style.transform = 'scale(1)';
         });
     });
+});
+
+window.addEventListener('load', function() {
+    var backImage = document.querySelector('.back-image');
+    backImage.classList.add('loaded');
 });
